5de1e33a8c8c92b1f9befd5ec0dae774c6a25808
Vitalik Buterin <VitalikZhou@gmail.com>
2023-05-08 20:12:34 +0800

    undigraph.sol

---
+++ undigraph.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+contract Undigraph {
+    // TODO 数据结构自己设计
+
+    function addEdge(uint256 vertext1, uint256 vertext2) external {
+        // TODO
+    }
+
+    function BFS(uint256 start) external returns (uint256[] memory) {
+        // TODO
+    }
+
+    function DFS(uint256 start) external returns (uint256[] memory) {
+        // TODO
+    }
+
+    function ShortestPath(uint256 start) external returns (uint256[] memory) {
+        // TODO
+    }
+}
